7786b08643310d46b9f6811bb1d9f4c8ee3f0e3a
Lucas Santos <lucas.santos@faculdadeiv2.com.br>
2020-10-05 10:57:41 -0300

    aaaaa

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'))
+app.use(express.static(__dirname))
 
 app.listen(3500, () => {
     console.log("app online on port 3500");
